2dc8bb8e42d570fcedbec274ea26015bacf10f10
hattima-tim <irfanridwan20@gmail.com>
2023-07-03 22:11:26 +0600

    change route name

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/books', async function(req, res, next) {
   const {PrismaClient} = require('@prisma/client');
   const prisma = new PrismaClient();
 

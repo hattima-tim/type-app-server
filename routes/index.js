a20c44f9673dcd6f3b1fa955affdcd4166384f2d
hattima-tim <irfanridwan20@gmail.com>
2023-07-03 22:02:48 +0600

    send books data

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const {PrismaClient} = require('@prisma/client');
+  const prisma = new PrismaClient();
+
+  const allBooks= await prisma.books.findMany()
+  
+  res.json(allBooks)
 });
 
 module.exports = router;

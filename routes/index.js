f6917cc1086cb6b230643d9550506084e877dd26
hattima-tim <irfanridwan20@gmail.com>
2023-07-04 12:02:55 +0600

    make prisma setup reusable

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
 
 /* GET home page. */
 router.get('/books', async function(req, res, next) {
-  const {PrismaClient} = require('@prisma/client');
-  const prisma = new PrismaClient();
-
   const allBooks= await prisma.books.findMany()
   
   res.json(allBooks)

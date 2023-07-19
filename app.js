4b002b5ed0b5225fbc93a71a804191c4c3927f73
hattima-tim <irfanridwan20@gmail.com>
2023-07-19 22:08:36 +0600

    replace all vars to const

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const cors = require("cors");
 const session = require("express-session");
 const passport = require("passport");
@@ -11,12 +11,12 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-var indexRouter = require("./routes/index");
-var userRouter = require("./routes/user");
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
 const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));

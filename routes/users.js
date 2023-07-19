df0e95920f9a60eae589f8d4cae69f82de922edf
hattima-tim <irfanridwan20@gmail.com>
2023-07-19 22:06:24 +0600

    sort and send all users username,wpm

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
+router.get("/leaderboard", async (req, res, next) => {
+  const users = await prisma.users.findMany({
+    select: {
+      username: true,
+      wpm: true,
+    },
+    orderBy: {
+      wpm: "desc",
+    },
+  });
+
+  res.json(users);
+});
 module.exports = router;
